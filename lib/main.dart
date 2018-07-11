d3bc71f0219e45a676c192f1fee7bc88fac63424
probuse <nprobuse@gmail.com>
2018-07-11 10:28:05 +0300

    Add ListTile with Icon and startup Names from word_pair library

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Hello Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Startup Names'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
@@ -28,9 +21,64 @@ class RandomWords extends StatefulWidget{
 
 
 class RandomWordsState extends State<RandomWords>{
+  final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
+  final _biggerFont = TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions(){
+    return ListView.builder(
+      padding: EdgeInsets.all(16.0),
+
+        itemBuilder: (BuildContext context, int i){
+          // Add a one-pixel-high divider widget before each row in the ListView
+          if(i.isOdd) return Divider();
+
+          // 'i ~/ 2' divides i by 2 and returns an integer result
+          // This calculates the actual number of word pairings in the ListView
+          // minus the divider widgets
+          final index = i ~/ 2;
+          // If you have reached the end  of the available word pairings...
+          // ...generate 10 more and add them to the suggestions list.
+          if(index >= _suggestions.length){
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        },
+    );
+  }
+
+  Widget _buildRow(WordPair wordPair){
+    final alreadySaved = _saved.contains(wordPair);
+    print(alreadySaved); // TODO remove this line once your curiosity is satisfied
+    return ListTile(
+      title: Text(
+        wordPair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(wordPair);
+          }else{
+            _saved.add(wordPair);
+          }
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 4.0,
+        title: Text("Startup Name Generator"),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 }
