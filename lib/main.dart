d4aed77cf9757a32618720dd47787d2b08cdd8d5
probuse <nprobuse@gmail.com>
2018-07-11 07:23:45 +0300

    use english_words library, add RandomWords StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Hello Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Startup Names'),
         ),
-        body: Text('Hello There'),
+        body: Center(
+          child: RandomWords(),
+        ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget{
+  @override
+  createState() => RandomWordsState();
+}
+
+
+class RandomWordsState extends State<RandomWords>{
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
